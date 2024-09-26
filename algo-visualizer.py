653273de122dd7bea3b3f9e87400faddb7c8a339
adriancancode <vazqueza2004@gmail.com>
2024-09-26 11:05:13 -0700

    Implement quick sort and radix sort

--- algo-visualizer.py
+++ algo-visualizer.py
@@ -33,9 +33,68 @@ def merge_sort(arr):
             k += 1
     return arr
 
-# def quick_sort(arr):
+def partition(array, low, high):
+    pivot = array[high]
+    i = low - 1
 
-# def radix_sort(arr):
+    for j in range(low, high):
+        if array[j] <= pivot:
+            i += 1
+            array[i], array[j] = array[j], array[i]
+
+    array[i+1], array[high] = array[high], array[i+1]
+    return i+1
+
+def quicksort(array, low=0, high=None):
+    if high is None:
+        high = len(array) - 1
+
+    if low < high:
+        pivot_index = partition(array, low, high)
+        quicksort(array, low, pivot_index-1)
+        quicksort(array, pivot_index+1, high)
+
+
+def countingSort(arr, exp1): 
+   
+    n = len(arr) 
+   
+    
+    output = [0] * (n) 
+   
+    
+    count = [0] * (10) 
+   
+    
+    for i in range(0, n): 
+        index = (arr[i]/exp1) 
+        count[int((index)%10)] += 1
+   
+    for i in range(1,10): 
+        count[i] += count[i-1] 
+   
+    
+    i = n-1
+    while i>=0: 
+        index = (arr[i]/exp1) 
+        output[ count[ int((index)%10) ] - 1] = arr[i] 
+        count[int((index)%10)] -= 1
+        i -= 1
+   
+    
+    i = 0
+    for i in range(0,len(arr)): 
+        arr[i] = output[i] 
+ 
+
+def radixSort(arr):
+ 
+    max1 = max(arr)
+ 
+    exp = 1
+    while max1 // exp > 0:
+        countingSort(arr,exp)
+        exp *= 10
 
 def linear_search(arr, target):
     for i in arr:
